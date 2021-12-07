1e594dd25601fe14928dc9d3575c2ad2a607ed2f
Kevin Su <kevinsu@outlook.co.nz>
2021-12-07 17:22:19 +1300

    cards

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
 import paris from '../public/images/paris.jpg';
 import sydney from '../public/images/sydney.jpg';
+import brisbane from '../public/images/brisbane.jpg';
+import hongkong from '../public/images/hongkong.jpg';
+import tokyo from '../public/images/tokyo.jpg';
 
 export default function Home() {
   return (
@@ -21,23 +24,39 @@ export default function Home() {
         </div>
       </div>
       <div className = {styles.containerWhite}>
-        <h1>Explore | Places near you</h1>
+        <h1>Explore | Discover new destinations</h1>
         <div className = {styles.grid}>
           <div className = {styles.card}>
             <Image src={sydney} alt="" />
-            <h1>pepe</h1>
+            <div className = {styles.cardText}>
+              <h2>Syndey</h2>
+              <h3>Australia</h3>
+              <p>10 km away</p>
+            </div>
           </div>
           <div className = {styles.card}>
-            <Image src={sydney} alt="" />
-            <h1>pepe</h1>
+            <Image src={brisbane} alt="" />
+            <div className = {styles.cardText}>
+              <h2>Brisbane</h2>
+              <h3>Australia</h3>
+              <p>452 km away</p>
+            </div>
           </div>
           <div className = {styles.card}>
-            <Image src={sydney} alt="" />
-            <h1>pepe</h1>
+            <Image src={hongkong} alt="" />
+            <div className = {styles.cardText}>
+              <h2>Hong Kong</h2>
+              <h3>Hong Kong</h3>
+              <p>4690 km away</p>
+            </div>
           </div>
           <div className = {styles.card}>
-            <Image src={sydney} alt="" />
-            <h1>pepe</h1>
+            <Image src={tokyo} alt="" />
+            <div className = {styles.cardText}>
+              <h2>Tokyo</h2>
+              <h3>Japan</h3>
+              <p>6942 km away</p>
+            </div>
           </div>
         </div>
       </div>
